cd25117fb6b8db91b49320d2a279aec16b0040f6
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-05-25 23:07:52 +0900

    refactor: create camera_config_t with designated initialization

--- src/main.cpp
+++ src/main.cpp
@@ -13,33 +13,34 @@ void setup()
   Serial.setDebugOutput(true);
   Serial.println();
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.jpeg_quality = 10;
-  config.fb_count = 2;
+  camera_config_t cameraConfig = {
+      .pin_pwdn = PWDN_GPIO_NUM,
+      .pin_reset = RESET_GPIO_NUM,
+      .pin_xclk = XCLK_GPIO_NUM,
+      .pin_sscb_sda = SIOD_GPIO_NUM,
+      .pin_sscb_scl = SIOC_GPIO_NUM,
+      .pin_d7 = Y9_GPIO_NUM,
+      .pin_d6 = Y8_GPIO_NUM,
+      .pin_d5 = Y7_GPIO_NUM,
+      .pin_d4 = Y6_GPIO_NUM,
+      .pin_d3 = Y5_GPIO_NUM,
+      .pin_d2 = Y4_GPIO_NUM,
+      .pin_d1 = Y3_GPIO_NUM,
+      .pin_d0 = Y2_GPIO_NUM,
+      .pin_vsync = VSYNC_GPIO_NUM,
+      .pin_href = HREF_GPIO_NUM,
+      .pin_pclk = PCLK_GPIO_NUM,
+
+      .xclk_freq_hz = 20000000,
+      .ledc_timer = LEDC_TIMER_0,
+      .ledc_channel = LEDC_CHANNEL_0,
+      .pixel_format = PIXFORMAT_JPEG,
+      .frame_size = FRAMESIZE_UXGA,
+      .jpeg_quality = 10,
+      .fb_count = 2};
 
   // camera init
-  esp_err_t err = esp_camera_init(&config);
+  esp_err_t err = esp_camera_init(&cameraConfig);
   if (err != ESP_OK)
   {
     Serial.printf("Camera init failed with error 0x%x", err);
